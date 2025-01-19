0004ca62248f070c1023e3cecd410ea00ec9227c
Oscar Bouteille <oscar_b@disroot.org>
2025-01-19 17:22:19 +0100

    Hello World

--- clear_tmp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-find /tmp -type f -delete
-
-exit 0
--- main.c
+++ main.c
@@ -5,19 +5,17 @@ int main(int argc, char *argv[])
 {
     int choix = 1;
     printf("Bienvenue sur Automatik. Ici, vous pourrez exécuter différents scripts très simplement.\n");
-    printf("Voulez vous : cleaner votre cache(1), genérer du lorem coloré avec lolcat(requis)(2) ou encore mettre un arbre de noël animé dans la console de votre système(3).\n");
+    printf("Voulez vous : genérer du lorem coloré avec lolcat(requis)(1).\n");
     scanf("%d", &choix);
-    if ( choix == 1 ){
+    /*if ( choix == 1 ){
         system("/home/oscar/Automatik/clear_tmp.sh");
-    }
-     if ( choix == 2 ){
+    }*/
+     if ( choix == 1 ){
  system("/home/oscar/Automatik/echo.sh");
-
     }
-     if ( choix == 3 ){
+     /*if ( choix == 3 ){
  system("/home/oscar/Automatik/christmas.sh");
-
-    }
+    }*/
 
 
   return 0;
