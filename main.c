ef3bf1dedf2771c4dda0026b5803d64a491f1ae0
Oscar Bouteille <oscar_b@disroot.org>
2025-01-20 18:35:14 +0100

    Hello everybody

--- main.c
+++ main.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <gtk-4.0/>
 
 int main(int argc, char *argv[])
 {
     int choix = 1;
     printf("Bienvenue sur Automatik. Ici, vous pourrez exécuter différents scripts très simplement.\n");
-    printf("Voulez vous : genérer du lorem coloré avec lolcat(requis)(1).\n");
+    printf("Voulez vous : cleanez votre cache utilisateur (1) genérer du lorem coloré avec lolcat(requis)(2).\n");
     scanf("%d", &choix);
-    /*if ( choix == 1 ){
-        system("/home/oscar/Automatik/clear_tmp.sh");
-    }*/
-     if ( choix == 1 ){
+      if ( choix == 1 ){
+        system("/home/oscar/Automatik/clear_cache.sh");
+    }
+     if ( choix == 2 ){
  system("/home/oscar/Automatik/echo.sh");
     }
      /*if ( choix == 3 ){
