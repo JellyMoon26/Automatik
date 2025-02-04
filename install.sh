4f663b3d84e4316091225e85d317c31e3b063ecc
Oscar Bouteille <oscar_b@disroot.org>
2025-02-04 16:05:57 +0100

    commit !

---
+++ install.sh
@@ -0,0 +1,16 @@
+echo "Installation en cours …"
+
+
+gcc src/main.c -o automatik
+
+sudo cp automatik /usr/bin/automatik
+
+cd  ~
+
+mkdir .automatik
+
+cd Automatik
+
+cp -r scripts ~/.automatik/scripts
+
+
