1845c8292ab60a29f9f334b3c57e229ce9be36e1
Oscar Bouteille <oscar_b@disroot.org>
2025-02-10 13:26:20 +0100

    add a fontion

--- src/main.c
+++ src/main.c
@@ -6,12 +6,16 @@ int main(int argc, char *argv[])
     int choix = 1;
 
     printf("Bienvenue sur Automatik. Ici, vous pourrez exécuter différentes commandes amusantes ou utiles très simplement.\n");
-    printf("Voulez vous : cleanez votre cache utilisateur (1), genérer le répertoire de votre dossier personnel coloré avec lolcat(requis)(2), lister tous les fihiers de votre répertoire personnel avec de la couleur(3) ou encore afficher les informations de votre système linux(4).\n");
+
+    printf("Voulez vous :\n -cleanez votre cache utilisateur (1)\n -genérer le répertoire de votre dossier personnel coloré avec lolcat(requis)(2) \n -lister tous les fihiers de votre répertoire personnel avec de la couleur(3)\n -ou encore afficher les informations de votre système linux(4)\n -regarder Star Wars en caractères ASCII(pour quitter, tapez ^] et quit )(5)\n");
+
 
     scanf("%d", &choix);
 
+
+
     if ( choix == 1 ){
-system("rm -r ~/.cache");
+system("rm -r  ~/.cache");
     }
 
     if ( choix == 2 ){
@@ -24,6 +28,10 @@ system("ls -a ~ | lolcat");
 
     if (choix == 4){
 system("neofetch");
+}
+
+    if (choix == 5){
+    system("telnet towel.blinkenlights.nl");
 }
   return 0;
 }
