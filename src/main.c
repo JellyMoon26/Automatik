4b4ce3849746e5ac08c3ec43aac5feb8dedabbb9
Oscar Bouteille <oscar_b@disroot.org>
2025-02-11 17:37:18 +0100

    commit

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 
     printf("Bienvenue sur Automatik. Ici, vous pourrez exécuter différentes commandes amusantes ou utiles très simplement.\n");
 
-    printf("Voulez vous :\n -cleanez votre cache utilisateur (1)\n -genérer le répertoire de votre dossier personnel coloré avec lolcat(requis)(2) \n -lister tous les fihiers de votre répertoire personnel avec de la couleur(3)\n -afficher les informations de votre système linux(4)\n -regarder Star Wars en caractères ASCII(pour quitter, tapez ^] et quit )(5)\n -faire un feu dans votre bureau(6)\n -genérer un texte surprise en couleur(7)");
+    printf("Voulez vous :\n -cleanez votre cache utilisateur (1)\n -genérer le répertoire de votre dossier personnel coloré avec lolcat(requis)(2) \n -lister tous les fihiers de votre répertoire personnel avec de la couleur(3)\n -afficher les informations de votre système linux(4)\n -regarder Star Wars en caractères ASCII(pour quitter, tapez ^] et quit )(5)\n -faire un feu dans votre bureau(6)\n -genérer un texte surprise en couleur(7)\n -Quitter Automatik(10)");
 
 
     scanf("%d", &choix);
@@ -40,6 +40,10 @@ system("neofetch");
 
     if (choix == 7){
         system("toilet Automatik | lolcat");
+}
+
+   if (choix == 7412){
+         exit(EXIT_SUCCESS);
 }
   return 0;
 }
