5d521c9906603eca58a2f0f0306bbe821f06a583
Blacksmithsquirrel <oscar_b@disroot.org>
2025-02-16 14:45:52 +0100

    Commit. just a commit

--- src/main.c
+++ src/main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include<string.h>
 int main(int argc, char *argv[])
 {
     int choix = 1;
-    int boucle = 1;
 
     printf("Bienvenue sur Automatik. Ici, vous pourrez exécuter différentes commandes amusantes ou utiles très simplement.\n");
+
+    printf()
 
     printf("Voulez vous :\n -cleanez votre cache utilisateur (1)\n -genérer le répertoire de votre dossier personnel coloré avec lolcat(requis)(2) \n -lister tous les fihiers de votre répertoire personnel avec de la couleur(3)\n -afficher les informations de votre système linux(4)\n -regarder Star Wars en caractères ASCII(pour quitter, tapez ^] et quit )(5)\n -faire un feu dans votre bureau(6)\n -genérer un texte surprise en couleur(7)\n -Quitter Automatik(10)\n");
 
